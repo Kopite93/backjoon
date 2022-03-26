3e71862826c58037f80e50def6e3d2ec8229ba79
Kopite93 <1000nulee@naver.com>
2022-03-27 00:40:42 +0900

    220326

---
+++ 2577.js
@@ -0,0 +1,16 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+function solution(input) {
+  let multi = String(input[0] * input[1] * input[2]);
+  for (let i = 0; i <= 9; i++) {
+    let count = 0;
+
+    for (let j = 0; j < multi.length; j++) {
+      if (Number(multi[j]) === i) {
+        count++;
+      }
+    }
+    console.log(count);
+  }
+}
